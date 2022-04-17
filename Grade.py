ef06a87804cfca4eb50f366490b98137052308d1
MeganHarpring <Harpring.megan13@gmail.com>
2022-04-16 21:58:06 -0400

    Final commit

---
+++ Grade.py
@@ -0,0 +1,35 @@
+#What is the score?
+score=int(input("What is your test score?"))
+
+#Determine the letter grade.
+if score>=90:
+    print('Your grade is an A.')
+else:
+    if score>=80:
+        print('Your grade is an B.')
+    else:
+        if score>=70:
+            print('Your grade is an C.')
+        else:
+            if score>=60:
+                print('Your grade is an D.')
+            else:
+                print('Your grade is an F.')
+
+
+#Alternatively you can use the 'if-elif-else' statement to make it easier to read:
+#What is the score?
+score=int(input("What is your test score?"))
+
+#Determine the letter grade.
+if score>=90:
+    print('Your grade is an A.')
+elif score>=80:
+    print('Your grade is an B.')
+elif score>=70:
+    print('Your grade is an C.')
+elif score>=60:
+    print('Your grade is an D.')
+else:
+    print('Your grade is an F.')
+    
